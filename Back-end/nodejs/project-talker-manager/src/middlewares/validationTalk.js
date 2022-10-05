62cc86315038a343fcd7286025a08699f41fb11c
leandro-miranda <leandromiranda2010@gmail.com>
2022-10-05 17:31:41 -0300

    validando talk

---
+++ Back-end/nodejs/project-talker-manager/src/middlewares/validationTalk.js
@@ -0,0 +1,11 @@
+const validationTalk = (req, res, next) => {
+  const { talk } = req.body;
+
+  if (!talk) {
+    return res.status(400).json({ message: 'O campo "talk" é obrigatório' });
+  }
+
+  next();
+};
+
+module.exports = validationTalk;
