6a72b97818e985103222b68ea1700718f580e907
leandro-miranda <leandromiranda2010@gmail.com>
2022-10-05 18:11:57 -0300

    validation rate

--- Back-end/nodejs/project-talker-manager/src/middlewares/validationRate.js
+++ Back-end/nodejs/project-talker-manager/src/middlewares/validationRate.js
@@ -1,12 +1,12 @@
 const validationRate = (req, res, next) => {
   const { talk: { rate } } = req.body;
 
-  if (!rate || rate === '') {
-    return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
-  }
-
   if (rate < 1 || rate > 5) {
     return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  
+  if (!rate || rate === '') {
+    return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
   }
 
   next();
