9a8c9c220dc14bb82b931bb8f5180fbe85d7aec0
leandro-miranda <leandromiranda2010@gmail.com>
2022-10-05 17:09:51 -0300

    importando o validationLogin

--- Back-end/nodejs/project-talker-manager/src/routers/login.js
+++ Back-end/nodejs/project-talker-manager/src/routers/login.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const validationLogin = require('../middlewares/validationLogin');
 const validationToken = require('../middlewares/validationToken');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', validationLogin, (req, res) => {
   const token = validationToken();
   res.status(200).json({ token });
 });
