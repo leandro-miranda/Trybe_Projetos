7f69d7178ddc147f658c08b05fce423b136adb9e
leandro-miranda <leandromiranda2010@gmail.com>
2022-10-05 17:31:05 -0300

    validando watchedAt

---
+++ Back-end/nodejs/project-talker-manager/src/middlewares/validationWatchedAt.js
@@ -0,0 +1,17 @@
+const validationWatchedAt = (req, res, next) => {
+  const { talk: { watchedAt } } = req.body;
+
+  if (!watchedAt) {
+    return res.status(400).json({ message: 'O campo "watchedAt" é obrigatório' });
+  }
+
+  const dataRegex = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i; // https://stackoverflow.com/questions/62960834/regex-date-dd-mm-yyyy
+
+  if (!dataRegex.test(watchedAt)) {
+    return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+
+  next();
+};
+
+module.exports = validationWatchedAt;
