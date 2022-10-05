0e847842386a711d13aefa175866f45b1ecd1248
leandro-miranda <leandromiranda2010@gmail.com>
2022-10-05 15:47:47 -0300

    writeFile ok

---
+++ Back-end/nodejs/project-talker-manager/src/utils/writeFiles.js
@@ -0,0 +1,7 @@
+const { writeFile } = require('fs').promises;
+
+const writeFiles = async (talker) => {
+  await writeFile('./src.talker.json', JSON.stringify(talker));
+};
+
+module.exports = writeFiles;
