1143036cd7b27bd516ffd4d1b46ca06689a76927
leandro-miranda <leandromiranda2010@gmail.com>
2022-10-05 16:21:47 -0300

    endpoint get /talker/:id

--- Back-end/nodejs/project-talker-manager/src/routers/talker.js
+++ Back-end/nodejs/project-talker-manager/src/routers/talker.js
@@ -12,4 +12,16 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const result = await readFiles();
+  const person = result.findIndex((index) => index.id === +(id));
+  
+  if (person < 0) {
+    res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  } else {
+    res.status(200).json(result[person]);
+  }
+});
+
 module.exports = router;
