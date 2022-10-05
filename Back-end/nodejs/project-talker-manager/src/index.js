f258a991e07781e1a43ed37cd7427593c601da66
leandro-miranda <leandromiranda2010@gmail.com>
2022-10-05 16:14:38 -0300

    importando talker

--- Back-end/nodejs/project-talker-manager/src/index.js
+++ Back-end/nodejs/project-talker-manager/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const talker = require('./routers/talker');
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,6 +12,8 @@ const PORT = '3000';
 app.get('/', (_req, res) => {
   res.status(HTTP_OK_STATUS).send();
 });
+
+app.use('/talker', talker);
 
 app.listen(PORT, () => {
   console.log('Online');
